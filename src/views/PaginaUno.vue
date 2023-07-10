e2d286ca2b52c6905bac5fbae09d475626fceb5c
LuisEnriqueCortijoGonzales <84096868+LuisEnriqueCortijoGonzales@users.noreply.github.com>
2023-07-09 19:56:11 -0500

    primer commit

---
+++ src/views/PaginaUno.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-conainer">
+  <div class="pagina">
+    <h2 class="titulo">Página 1</h2>
+    <!-- Contenido de la página 1 -->
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.page-container{
+.pagina {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to bottom right, #000000, #ffffff);
+  color: #ffffff;
+  font-family: Arial, sans-serif;
+}
+
+.titulo {
+  font-size: 32px;
+}
+}
+</style>
